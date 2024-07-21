0f1901b509b7906219f2f9d9a0712ddfa333b330
ProfAndersonAndrade <andersonandrades@prof.educacao.sp.gov.br>
2024-07-21 13:09:55 -0300

    ubu atualização

---
+++ analise_combinatoria.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+
+st.title('Análise combinatória')
+st.text('''
+A Análise combinatória é a área que visa contar elementos em certas
+            condições de agrupamentos.
+''')
+
+
